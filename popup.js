8399d05bfd29a9a41b214327766bb3b8d3074d48
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2020-06-11 20:14:05 -0700

    made popup menu somewhat pretty

--- popup.js
+++ popup.js
@@ -3,20 +3,20 @@ var slider = document.getElementById('threshold-slider');
 
 // pull threshold value from chrome storage API
 chrome.storage.sync.get("global", function(globalData) {
-    slider.value = globalData.global.threshold;
+    slider.value = globalData.global.threshold*100;
     display.innerHTML = String(globalData.global.threshold*100) + '%';  
 });
 
 // visually update slider readout
 slider.oninput = function() {
-    display.innerHTML = String(slider.value*100) + '%';
+    display.innerHTML = String(Math.round(slider.value)) + '%';
 };
 
 // update chrome storage API with updated threshold value
 slider.onchange = function() {
     chrome.storage.sync.get("global", function(globalData) {
         var obj = globalData;
-        obj.global.threshold = Number(slider.value);
+        obj.global.threshold = Number(slider.value/100);
         chrome.storage.sync.set(obj);
     });
 };
